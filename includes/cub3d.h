f18b2c30152c65ce77fe728abc4ed1d0799c572a
pnovato <ptrck.nvt.slv@gmail.com>
2026-03-24 13:50:06 +0000

    Add: Adding t_texture struct n it's fields

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -10,12 +10,32 @@
 # define WIDTH 1920
 # define HEIGHT 1080
 
+typedef enum e_dir
+{
+    N,
+    S,
+    W,
+    E
+}   t_dir;
+
 typedef struct s_cub
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	void	*img_ptr;
 }	t_cub;
 
+typedef struct s_texture
+{
+	char		*addr_img;
+	void		*img_ptr;
+	int		bits_p_pixel;
+	int		size_line;
+	int		endian;
+	int		t_height;
+	int		t_width;
+}	t_texture;
+	
 
 // init_mlx.c
 void	init_window(t_cub *vars);
